88c715673d0bd6b6cb654ca5031ac7db8d069106
neefr1k <leryuic@gmail.com>
2026-04-20 21:54:32 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 def login(user, password):
+    # валидация
     if not user or not password:
         print("Ошибка: пустые данные")
         return False
 
+    # бизнес-логика
     if password == "1234":
         print("Успешный вход")
         return True
     else:
         print("Неверный пароль")
         return False
+
+
+def get_user_status(user):
+    # новая функция
+    if user == "admin":
+        return "Администратор"
+    elif user:
+        return "Обычный пользователь"
+    else:
+        return "Гость"
+
+
+# запуск программы
+user = input("Введите имя: ")
+password = input("Введите пароль: ")
+
+if login(user, password):
+    status = get_user_status(user)
+    print("Статус:", status)
